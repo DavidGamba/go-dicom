e23346d19e6e53c02e1604fb0912306ea0029fb6
David Gamba <davidgamba@gmail.com>
2016-03-01 21:40:59 -0700

    Add extra SOP Classes to list

--- sopclass/sopclass.go
+++ sopclass/sopclass.go
@@ -6,7 +6,33 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// Package sopclass contains constants for DICOM SOP classes
 package sopclass
 
-// VerificationSOPClass = "1.2.840.10008.1.1"
+// VerificationSOPClass Verification SOP Class
+// 1.2.840.10008.1.1
 const VerificationSOPClass = "1.2.840.10008.1.1"
+
+// PatientRootQRIMFind Patient Root Query/Retrieve Information Model – FIND
+// 1.2.840.10008.5.1.4.1.2.1.1
+const PatientRootQRIMFind = "1.2.840.10008.5.1.4.1.2.1.1"
+
+// PatientRootQRIMMove Patient Root Query/Retrieve Information Model – MOVE
+// 1.2.840.10008.5.1.4.1.2.1.2
+const PatientRootQRIMMove = "1.2.840.10008.5.1.4.1.2.1.2"
+
+// PatientRootQRIMGet Patient Root Query/Retrieve Information Model – GET
+// 1.2.840.10008.5.1.4.1.2.1.3
+const PatientRootQRIMGet = "1.2.840.10008.5.1.4.1.2.1.3"
+
+// StudyRootQRIMFind Study Root Query/Retrieve Information Model – FIND
+// 1.2.840.10008.5.1.4.1.2.2.1
+const StudyRootQRIMFind = "1.2.840.10008.5.1.4.1.2.2.1"
+
+// StudyRootQRIMMove Study Root Query/Retrieve Information Model – MOVE
+// 1.2.840.10008.5.1.4.1.2.2.2
+const StudyRootQRIMMove = "1.2.840.10008.5.1.4.1.2.2.2"
+
+// StudyRootQRIMGet Study Root Query/Retrieve Information Model – GET
+// 1.2.840.10008.5.1.4.1.2.2.3
+const StudyRootQRIMGet = "1.2.840.10008.5.1.4.1.2.2.3"
--- syntax/ts/ts.go
+++ syntax/ts/ts.go
@@ -6,6 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// Package ts contains DICOM transfer syntax constants
 package ts
 
 // ImplicitVRLittleEndian = "1.2.840.10008.1.2"
